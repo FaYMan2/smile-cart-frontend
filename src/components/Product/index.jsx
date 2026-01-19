a3daf745f7a287fed45ffbfc385ea9be9824d4df
FaYMan2 <suavarnavats244@gmail.com>
2026-01-19 18:17:21 +0530

    Added ability to set quantity of cart items

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -34,7 +34,15 @@ const Product = () => {
     fetchProduct();
   }, []);
 
-  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
+  const {
+    name,
+    description,
+    mrp,
+    offerPrice,
+    imageUrls,
+    imageUrl,
+    availableQuantity,
+  } = product;
 
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
@@ -67,7 +75,7 @@ const Product = () => {
           <p className="font-semibold text-green-600">
             {discountPercentage}% off
           </p>
-          <AddToCart {...{ slug }} />
+          <AddToCart {...{ availableQuantity, slug }} />
         </div>
       </div>
     </div>
---
+++ src/components/commons/ProductQuantity.jsx
@@ -0,0 +1,75 @@
+import { useRef } from "react";
+
+import { VALID_COUNT_REGEX } from "components/constants";
+import useSelectedQuantity from "components/hooks/useSelectedQuantity";
+import { Button, Input } from "neetoui";
+import { toast } from "react-toastify";
+
+import TooltipWrapper from "./TooltipWrapper";
+
+const ProductQuantity = ({ availableQuantity, slug }) => {
+  const [selectedQuantity, setSelectedQuantity] = useSelectedQuantity(slug);
+  const isNotValidQuantity = selectedQuantity >= availableQuantity;
+  const inputRef = useRef();
+
+  const preventNavigation = e => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleSetCount = event => {
+    const { value } = event.target;
+    const isNotValidInputQuantity = parseInt(value) > availableQuantity;
+
+    if (isNotValidInputQuantity) {
+      toast.error(`Only ${availableQuantity} units are available`, {
+        autoClose: 2000,
+      });
+      setSelectedQuantity(availableQuantity);
+      inputRef.current.blur();
+    } else if (VALID_COUNT_REGEX.test(value)) {
+      setSelectedQuantity(value);
+    }
+  };
+
+  return (
+    <div className="neeto-ui-border-black neeto-ui-rounded inline-flex flex-row items-center border">
+      <Button
+        className="focus-within:ring-0"
+        label="-"
+        style="text"
+        onClick={e => {
+          preventNavigation(e);
+          setSelectedQuantity(String(parseInt(selectedQuantity) - 1));
+        }}
+      />
+      <Input
+        nakedInput
+        className="ml-2"
+        contentSize="2"
+        ref={inputRef}
+        value={selectedQuantity}
+        onChange={handleSetCount}
+        onClick={preventNavigation}
+      />
+      <TooltipWrapper
+        content="Reached maximum units"
+        position="top"
+        showTooltip={isNotValidQuantity}
+      >
+        <Button
+          className="focus-within:ring-0"
+          disabled={isNotValidQuantity}
+          label="+"
+          style="text"
+          onClick={e => {
+            preventNavigation(e);
+            setSelectedQuantity(String(parseInt(selectedQuantity) + 1));
+          }}
+        />
+      </TooltipWrapper>
+    </div>
+  );
+};
+
+export default ProductQuantity;
