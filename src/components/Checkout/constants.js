d941667a935ab13197a3b2e9ff60dbc2135cedca
FaYMan2 <suavarnavats244@gmail.com>
2026-01-20 18:17:06 +0530

    Added submission handler to forms

--- src/components/Checkout/constants.js
+++ src/components/Checkout/constants.js
@@ -16,8 +16,8 @@ export const CHECKOUT_FORM_INITIAL_VALUES = {
 export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
   email: yup
     .string()
-    .email(t("validations.emailInvalid"))
-    .required(t("validations.emailRequired")),
+    .email(() => t("validations.emailInvalid"))
+    .required(() => t("validations.emailRequired")),
   country: yup
     .object()
     .shape({
@@ -25,11 +25,11 @@ export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
       code: yup.string().required(),
     })
     .nullable(),
-  firstName: yup.string().required(t("validations.firstNameRequired")),
-  lastName: yup.string().required(t("validations.lastNameRequired")),
-  address: yup.string().required(t("validations.addressRequired")),
-  apartment: yup.string().required(t("validations.apartmentRequired")),
-  city: yup.string().required(t("validations.cityRequired")),
+  firstName: yup.string().required(() => t("validations.firstNameRequired")),
+  lastName: yup.string().required(() => t("validations.lastNameRequired")),
+  address: yup.string().required(() => t("validations.addressRequired")),
+  apartment: yup.string().required(() => t("validations.apartmentRequired")),
+  city: yup.string().required(() => t("validations.cityRequired")),
   state: yup
     .object()
     .shape({
@@ -37,6 +37,6 @@ export const CHECKOUT_FORM_VALIDATION_SCHEMA = yup.object().shape({
       code: yup.string().required(),
     })
     .nullable()
-    .required(t("validations.stateRequired")),
-  zipCode: yup.number().required(t("validations.zipCodeRequired")),
+    .required(() => t("validations.stateRequired")),
+  zipCode: yup.number().required(() => t("validations.zipCodeRequired")),
 });
