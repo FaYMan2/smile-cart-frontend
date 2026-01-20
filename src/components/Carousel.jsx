22a7a838c770b264a3829fbf995176caeb3962c4
FaYMan2 <suavarnavats244@gmail.com>
2026-01-20 18:55:27 +0530

    Enhanced app performance using memoization

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, memo } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -74,4 +74,4 @@ const Carousel = ({ imageUrls, title }) => {
   );
 };
 
-export default Carousel;
+export default memo(Carousel);
